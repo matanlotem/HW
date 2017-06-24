baf87a6bc309dc3f9e7c7b3238fcf67ae6cda9f1
matanlotem <lotemmtn@gmail.com>
2017-06-24 22:06:40 +0300

    message_slot - device list data structure

--- HW5/message_slot.c
+++ HW5/message_slot.c
@@ -7,19 +7,127 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <asm/uaccess.h>
-#include <linux/string.h>
+//#include <linux/string.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
 
 #define SUCCESS 0
 #define DEVICE_RANGE_NAME "message_slot"
-#define BUF_LEN 120
 #define DEVICE_FILE_NAME "simple_message_slot"
+#define BUFFER_LEN 120
+#define NUM_BUFFERS 4
 
+#define ERR_PREFIX "MessageSlot ERROR: "
+#define ALLOCATION_ERROR ERR_PREFIX "Allocation error!\n"
+#define DEVICE_OPEN_ERROR ERR_PREFIX "Error opening device\n"
+#define DEVICE_NOT_FOUND ERR_PREFIX "Device not found\n"
+
+
+
+/* ********** ********** ********** ********** ********** ********** ********** */
+/* ********** ********** ********** DEVICE_DATA ********* ********** ********** */
+/* ********** ********** ********** ********** ********** ********** ********** */
+typedef struct device_data_t {
+	int ino;
+	struct device_data_t* next;
+	char* buffer;
+	char* all_buffers[NUM_BUFFERS];
+} device_data;
+
+static device_data* dev_list = NULL;
+static device_data* dev_list_last = NULL;
+
+int add_device_data(int ino);
+device_data* get_device_data(int ino);
+void destroy_device_data(device_data* dev);
+void destroy_device_list(void);
+
+
+int add_device_data(int ino) {
+	// create device data
+	device_data* dev = (device_data*) kmalloc(sizeof(struct device_data_t),GFP_KERNEL);
+	if (!dev) {
+		printk(ALLOCATION_ERROR);
+		return -1;
+	}
+
+	// set paramaters
+	dev->ino = ino;
+	dev->next = NULL;
+	dev->buffer = NULL;
+
+	// allocate buffers
+	for (int i=0; i<NUM_BUFFERS; i++) {
+		dev->all_buffers[i] = (char*) kmalloc(sizeof(char) * BUFFER_LEN,GFP_KERNEL);
+		if (!dev->all_buffers[i]) { // allocation error
+			printk(ALLOCATION_ERROR);
+			destroy_device_data(dev);
+			return -1;
+		}
+	}
+
+	// place at end of list
+	if (!dev_list)
+		dev_list = dev;
+	else
+		dev_list_last->next = dev;
+	dev_list_last = dev;
+
+	return 0;
+}
+
+device_data* get_device_data(int ino) {
+	if (dev_list) {
+		// scan all devices and return if ino is found
+		device_data *dev = dev_list;
+		while (dev->next != NULL) {
+			if (dev->ino == ino)
+				return dev;
+			dev = dev->next;
+		}
+	}
+	// ino not found
+	return NULL;
+}
+
+
+void destroy_device_data(device_data* dev) {
+	if (dev) {
+		// free buffers
+		for (int i=0; i<NUM_BUFFERS; i++)
+			if (dev->all_buffers[i])
+				kfree(dev->all_buffers[i]);
+		// free device data
+		kfree(dev);
+	}
+}
+
+void destroy_device_list(void) {
+	device_data *next, *dev = dev_list;
+	while (dev != NULL) {
+		next = dev->next;
+		destroy_device_data(dev);
+		dev = next;
+	}
+}
+
+
+/* ********** ********** ********** ********** ********** ********** ********** */
+/* ********** ********** ******** MODULE METHODS ******** ********** ********** */
+/* ********** ********** ********** ********** ********** ********** ********** */
 static int major;
 
 static int device_open(struct inode *inode, struct file *file) {
-	printk("device_open(%p)\n", file);
+	printk("Device Open(%p)\n", file);
+	int ino = file->f_inode->i_ino;
+	if (!get_device_data(ino)) {
+		if (add_device_data(ino) != 0) {
+			printk(DEVICE_OPEN_ERROR);
+			return -ERANGE;
+		}
+	}
+
 	return SUCCESS;
 }
 
@@ -60,6 +168,7 @@ static int __init message_slot_init(void) {
 
 static void __exit message_slot_cleanup(void) {
 	printk("Unloading message_slot\n");
+	destroy_device_list();
 	unregister_chrdev(major, DEVICE_RANGE_NAME);
 }
 
--- HW5/test.sh
+++ HW5/test.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 DEVICE_RANGE_NAME=message_slot
 LOC=~/oslocal/HW/HW5/
-
-# prepare
-sudo dmesg --clear
 cd $LOC
 
-# load module
-sudo insmod $DEVICE_RANGE_NAME.ko
-# get major
-DEV_ROW=$(cat /proc/devices | grep "[0-9]* $DEVICE_RANGE_NAME")
-MAJOR_NUM=$(echo "$DEV_ROW" | grep -o [0-9]*)
-# create device file
-sudo mknod /dev/simple_message_slot c $MAJOR_NUM 0
+if [ "$#" == 0 ] || [ "$1" == "init" ]; then
+    # prepare
+    sudo dmesg --clear
+    # load module
+    sudo insmod $DEVICE_RANGE_NAME.ko
+    # get major
+    DEV_ROW=$(cat /proc/devices | grep "[0-9]* $DEVICE_RANGE_NAME")
+    MAJOR_NUM=$(echo "$DEV_ROW" | grep -o [0-9]*)
+    # create device file
+    sudo mknod /dev/simple_message_slot c $MAJOR_NUM 0
+fi
 
 # run message sender
-./message_sender
+if [ "$#" == 0 ] || [ "$1" == "send" ]; then
+    ./message_sender
+fi
 
 #clean up
-sudo rm -f /dev/simple_message_slot
-sudo rmmod message_slot
+if [ "$#" == 0 ] || [ "$1" == "close" ]; then
+    sudo rm -f /dev/simple_message_slot
+    sudo rmmod message_slot
+fi
 
 # print output
 dmesg
